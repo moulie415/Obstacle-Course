5599201488d94a46ec59a03bcb8e1a1a9f61e1bf
Henry Moule <hen10@hotmail.co.uk>
2023-02-09 23:35:50 +0000

    obstacle changes

--- Assets/Scripts/Dropper.cs
+++ Assets/Scripts/Dropper.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Dropper : MonoBehaviour
 {
+    MeshRenderer renderer;
+    Rigidbody rigidBody;
+    [SerializeField] float timeToWait = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        renderer = GetComponent<MeshRenderer>();
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.useGravity = false;
+        renderer.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time > timeToWait)
+        {
+            renderer.enabled = true;
+            rigidBody.useGravity = true;
+            Debug.Log("3 seconds has elapsed");
+        }
     }
 }
